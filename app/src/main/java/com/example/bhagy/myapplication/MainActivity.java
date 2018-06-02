c548c5ce3f907dc0e147d17c55227ef4d7418178
DESKTOP-SNL9V91\bhagy <chinthalapalli6@gmail.com>
2018-06-02 19:48:36 -0400

    Hashmap example.

--- app/src/main/java/com/example/bhagy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bhagy/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Random;
 
 import butterknife.BindView;
@@ -42,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<Employee> employees = new ArrayList<>();
     ArrayList<Integer> number = new ArrayList<>();
+    //hashmap example
+    HashMap<String,Integer> map = new HashMap<>();
     static int numberFound = -1;
     Random random = new Random();
     private static String[] result;
@@ -66,6 +69,18 @@ public class MainActivity extends AppCompatActivity {
 
         //sorting and searching example
         searchSort();
+
+
+        map.put("student1", 1);
+        map.put("student2",2);
+        map.put("student3", 3);
+        map.put("student4",4);
+        map.remove("student2");
+        Log.d("MainActivitymap", "Get student 4: " + map.get("student4").toString());
+        Log.d("MainActivitymap",keyInHashMap());
+        for(int i :map.values()){
+            Log.d("MainActivitymap",String.valueOf(i));
+        }
         saveButton.setOnClickListener(btnListener);
         getButton.setOnClickListener(btnListener);
         clearButton.setOnClickListener(btnListener);
@@ -124,13 +139,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
+//print numbers
     public static void printNumbers(ArrayList<Integer> nums) {
         for (int i = 0; i < nums.size(); i++) {
             Log.d("RandomNumbers", nums.get(i).toString());
         }
     }
-
+//print employee data
     public static void printEmployess(ArrayList<Employee> employeesArrayList) {
         for (Employee e : employeesArrayList) {
             Log.d("Employees", e.getEmployee());
@@ -148,6 +163,11 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return numberFound;
+    }
+    //check whether key present in  Hashmap
+    public String keyInHashMap(){
+       return  (map.containsKey("student2")?map.get("student2").toString():"null");
+
     }
 }
 
